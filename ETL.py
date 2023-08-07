ccb02fd613c7f96baeac06fb498118892fe4e6a9
LuisEuqor <120050448+LuisEuqor@users.noreply.github.com>
2023-08-07 03:06:18 -0600

    Update ETL.py

--- ETL.py
+++ ETL.py
@@ -3,15 +3,13 @@ import requests
 import pandas as pd 
 import json 
 import datetime
-#Creamos nuestros diccionarios ylistas con los APIs  y las columnas 
-api_urls=["https://rickandmortyapi.com/api/character","https://rickandmortyapi.com/api/character?page=2","https://rickandmortyapi.com/api/character?page=3","https://rickandmortyapi.com/api/character?page=4","https://rickandmortyapi.com/api/character?page=5","https://rickandmortyapi.com/api/character?page=6","https://rickandmortyapi.com/api/character?page=7","https://rickandmortyapi.com/api/character?page=8","https://rickandmortyapi.com/api/character?page=9","https://rickandmortyapi.com/api/character?page=10","https://rickandmortyapi.com/api/character?page=11","https://rickandmortyapi.com/api/character?page=12","https://rickandmortyapi.com/api/character?page=13","https://rickandmortyapi.com/api/character?page=14","https://rickandmortyapi.com/api/character?page=15","https://rickandmortyapi.com/api/character?page=16","https://rickandmortyapi.com/api/character?page=17","https://rickandmortyapi.com/api/character?page=18","https://rickandmortyapi.com/api/character?page=19","https://rickandmortyapi.com/api/character?page=20","https://rickandmortyapi.com/api/character?page=21","https://rickandmortyapi.com/api/character?page=22","https://rickandmortyapi.com/api/character?page=23","https://rickandmortyapi.com/api/character?page=24","https://rickandmortyapi.com/api/character?page=25","https://rickandmortyapi.com/api/character?page=26","https://rickandmortyapi.com/api/character?page=27","https://rickandmortyapi.com/api/character?page=28","https://rickandmortyapi.com/api/character?page=29","https://rickandmortyapi.com/api/character?page=30","https://rickandmortyapi.com/api/character?page=31","https://rickandmortyapi.com/api/character?page=32","https://rickandmortyapi.com/api/character?page=33","https://rickandmortyapi.com/api/character?page=34","https://rickandmortyapi.com/api/character?page=35","https://rickandmortyapi.com/api/character?page=36","https://rickandmortyapi.com/api/character?page=37","https://rickandmortyapi.com/api/character?page=38","https://rickandmortyapi.com/api/character?page=39","https://rickandmortyapi.com/api/character?page=40","https://rickandmortyapi.com/api/character?page=41","https://rickandmortyapi.com/api/character?page=42"]
-columnas=["id name","status","species","type,","gender","origin","location","image","episode","url","created"]
+Ajuste=json.loads(open("aux.json").read()) #leer el archivo de ajuste
 
 #Creamos nuestra primera funcion en donde extaemos los datos de la API  
 def extraer(lista_urls):
     df_core=[]
     for url in lista_urls: #ciclo en donde se recorren los urls para obtener el total de los datos 
-        reponse=requests.get(url)
+        response=requests.get(url)
         if response.status_code==200:
             data=response.json()
             df=pd.DataFrame(data["results"])
@@ -35,10 +33,13 @@ def transformar(json):
         #Retornamos el Data-Frame
         return df
     
-#Se crea la funcion en donde se cargar     
+#Se crea la funcion en donde se cargan los datos y se genera el excel 
 def cargar(df):
     df.to_excel("Salida_de_datos.xlsx")
 
-extraccion=extraer(api_urls)
-transformacion=transformar(extraccion)
-cargar(transformacion)
+def etl():
+    
+    result_extraccion=extraer(Ajuste["api_urls"])
+    result_transformacion=transformar(result_extraccion)
+    cargar(result_transformacion)
+etl()
